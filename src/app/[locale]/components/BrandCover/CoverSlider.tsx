2fe589de2b7bc0109525bd3fbf4992bfc8979c85
leye195 <leye195@naver.com>
2025-07-19 17:07:24 +0900

    feat: add Coverflow&CoverSlider components

--- src/app/[locale]/components/BrandCover/CoverSlider.tsx
+++ src/app/[locale]/components/BrandCover/CoverSlider.tsx
@@ -1,17 +1,11 @@
 import Image from "next/image";
 import { useRef } from "react";
-import { EffectCoverflow } from "swiper/modules";
+
 import type { SwiperRef } from "swiper/react";
 import { Swiper, SwiperSlide } from "swiper/react";
-
 import { cn } from "@/utils/style";
 
 import "swiper/css";
-import "swiper/css/effect-coverflow";
-
-interface CoverflowProps {
-  className?: string;
-}
 
 const images = [
   "https://res.cloudinary.com/dumqfde1s/image/upload/v1745226611/cld-sample-4.jpg",
@@ -21,7 +15,11 @@ const images = [
   "https://res.cloudinary.com/dumqfde1s/image/upload/v1745226611/samples/coffee.jpg",
 ];
 
-export default function Coverflow({ className }: CoverflowProps) {
+interface CoverSliderProps {
+  className?: string;
+}
+
+export default function CoverSlider({ className }: CoverSliderProps) {
   const swiperRef = useRef<SwiperRef | null>(null);
 
   const handleClick = (index: number) => {
@@ -33,36 +31,22 @@ export default function Coverflow({ className }: CoverflowProps) {
 
   return (
     <Swiper
-      centeredSlides
-      className={cn("cover-flow w-full", className)}
-      coverflowEffect={{
-        rotate: 0,
-        depth: 200,
-        stretch: -45,
-        modifier: 1,
-        slideShadows: false,
-      }}
-      effect="coverflow"
-      loop
+      className={cn("cover-slider w-full", className)}
       loopAdditionalSlides={1}
-      modules={[EffectCoverflow]}
       ref={swiperRef}
       slidesPerView="auto"
+      spaceBetween={16}
     >
       {[...images, ...images].map((img, index) => {
         return (
           <SwiperSlide
-            className={cn(
-              "mx-[10px] min-h-[400px]! w-[30vw]!",
-              "max-md:min-h-[200px]! max-md:w-[40vw]!",
-            )}
+            className={cn("min-h-[280px]! w-[190px]!")}
             key={`card-${index + 1}`}
             onClick={() => handleClick(index)}
           >
             <div
               className={cn(
-                "relative min-h-[260px]! w-[30vw]! cursor-pointer",
-                "max-md:min-h-[200px]! max-md:w-[40vw]!",
+                "relative min-h-[280px]! w-[190px]! cursor-pointer",
               )}
             >
               <Image
--- src/app/[locale]/components/BrandCover/index.tsx
+++ src/app/[locale]/components/BrandCover/index.tsx
@@ -3,9 +3,11 @@
 import Section from "@/components/ui/section";
 import { cn } from "@/utils/style";
 
+import Coverflow from "./Coverflow";
+
 import "swiper/css";
 import "swiper/css/effect-coverflow";
-import Coverflow from "./Coverflow";
+import CoverSlider from "./CoverSlider";
 
 interface Props {
   className?: string;
@@ -16,7 +18,7 @@ export default function BrandCover({ className }: Props) {
     <Section
       className={cn(
         "section-2 relative mx-auto flex max-w-[2200px] flex-col items-center justify-center gap-[52px] py-15",
-        "max-sm:h-auto max-sm:justify-start max-sm:pt-[90px]",
+        "max-sm:h-auto max-sm:gap-[60px] max-sm:px-[20px] max-sm:py-[90px]",
         className,
       )}
     >
@@ -24,13 +26,14 @@ export default function BrandCover({ className }: Props) {
         <h2
           className={cn(
             "text-[50px] font-bold",
-            "max-xl:text-[40px] max-lg:text-center max-md:break-keep max-sm:text-[30px]",
+            "max-xl:text-[40px] max-lg:text-center max-md:break-keep max-sm:text-[28px]",
           )}
         >
           Not a Place. A Feeling
         </h2>
       </div>
-      <Coverflow />
+      <Coverflow className="max-sm:hidden!" />
+      <CoverSlider className="max-sm:block! sm:hidden!" />
     </Section>
   );
 }
