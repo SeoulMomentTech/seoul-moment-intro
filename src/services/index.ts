99cbdcc948d9329a34bac4d26a1edb60f72ba562
leye195 <leye195@naver.com>
2025-08-09 19:03:35 +0900

    feat: update api baseUrl

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,7 @@
 import ky from "ky";
 
 export const api = ky.create({
-  prefixUrl:
-    "http://ec2-3-38-251-105.ap-northeast-2.compute.amazonaws.com:9008",
+  prefixUrl: "https://api.seoulmoment.com.tw",
   headers: {
     "Content-Type": "application/json",
   },
